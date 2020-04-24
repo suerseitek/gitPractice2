8c47d1efdd49f0232bc568ec7266e0958db1f1b9
Seitek Suerkulov <suerseitek@gmail.com>
2020-04-24 15:57:47 -0400

    firstpractice

---
+++ src/day_1/gitgitgit.java
@@ -0,0 +1,10 @@
+package day_1;
+
+public class gitgitgit {
+    public static void main(String[] args) {
+        System.out.println("git add . ");
+        System.out.println("git commit - m");
+        System.out.println("git push");
+
+    }
+}
